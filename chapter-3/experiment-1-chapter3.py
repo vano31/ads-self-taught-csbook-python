92657623a231ebefc15b714e8b732821c34e74f2
vano31 <vanosike31@gmail.com>
2022-05-12 23:40:38 -0400

    Made file

---
+++ chapter-3/experiment-1-chapter3.py
@@ -0,0 +1,9 @@
+
+sorted_fruit_list = ["apple", "banana", "coconut", "durian", "eggplant"]
+
+def find_fruit(list, target_list):
+    first_index = 0
+    last_index = (len(target_list) - 1)
+    
+
+
